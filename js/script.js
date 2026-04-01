089747c1074ae9728490f81e71943dd29e4b4766
SALWA507 <salwaamzane30@gmail.com>
2026-04-01 10:21:55 +0100

    la partie html design de header

---
+++ js/script.js
@@ -0,0 +1,24 @@
+const container = document.getElementById("games-container");
+const buttons = document.querySelectorAll(".filter-btn");
+
+function displayGames(filteredGames) {
+  container.innerHTML = "";
+
+  filteredGames.forEach(game => {
+    const div = document.createElement("div");
+    div.classList.add("bg-white", "rounded-xl", "shadow", "p-3");
+
+    div.innerHTML = `
+      <img src="${game.image}" class="w-full h-40 object-cover rounded-lg" />
+      <h3 class="font-bold mt-2">${game.title}</h3>
+      <p>${game.description}</p>
+      <div class="flex justify-between mt-2">
+        <p>⭐ 4.8</p>
+        <p class="text-green-600 font-bold">${game.price}$</p>
+      </div>
+    `;
+
+    container.appendChild(div);
+  });
+}
+
