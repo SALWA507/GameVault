017f361153bd7f333604a6cfed656a6fdf8d8847
SALWA507 <salwaamzane30@gmail.com>
2026-04-03 16:14:28 +0100

    ajouter plus au moin de la quantitee

--- js/panier.js
+++ js/panier.js
@@ -1,40 +1,69 @@
 const container = document.getElementById("cart-container");
 const totalDisplay = document.getElementById("total");
-const orderBtn = document.getElementById("orderBtn");
-
 let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
 function displayCart() {
   container.innerHTML = "";
   let total = 0;
 
-  cart.forEach(item => {
-    const div = document.createElement("div");
-    div.className = "bg-white p-3 flex justify-between rounded shadow";
+  for (let i = 0; i < cart.length; i++) {
 
-    div.innerHTML = `
-      <p>${item.title} x ${item.quantity || 0}</p>
-      <p>${((item.price || 0) * (item.quantity || 0)).toFixed(2)}$</p>
-    `;
+    let item = cart[i];
+
+    let div = document.createElement("div");
+    div.className = "bg-white p-3 mb-2";
+
+    div.innerHTML =
+      item.title + " x " + item.quantity + " - " +
+      (item.price * item.quantity).toFixed(2) + "$" +
+
+      " <button onclick='increase(" + item.id + ")'>+</button>" +
+      " <button onclick='decrease(" + item.id + ")'>-</button>" +
+      " <button onclick='removeItem(" + item.id + ")'>X</button>";
 
     container.appendChild(div);
 
-    total += (item.price || 0) * (item.quantity || 0);
-  });
+    total += item.price * item.quantity;
+  }
 
   totalDisplay.innerText = "Total : " + total.toFixed(2) + "$";
 }
 
-displayCart();
 
-orderBtn.addEventListener("click", () => {
-  if(cart.length === 0) {
-    alert("Le panier est vide !");
-    return;
+function increase(id) {
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].id === id) {
+      cart[i].quantity++;
+    }
   }
 
-  alert("Commande réussie !");
-  cart = [];
   localStorage.setItem("cart", JSON.stringify(cart));
   displayCart();
-});
+}
+
+function decrease(id) {
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].id === id && cart[i].quantity > 1) {
+      cart[i].quantity--;
+    }
+  }
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
+}
+
+function removeItem(id) {
+  let newCart = [];
+
+  for (let i = 0; i < cart.length; i++) {
+    if (cart[i].id !== id) {
+      newCart.push(cart[i]);
+    }
+  }
+
+  cart = newCart;
+
+  localStorage.setItem("cart", JSON.stringify(cart));
+  displayCart();
+}
+
+displayCart();
